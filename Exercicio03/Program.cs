c45b4f115c71a2c9a74c119960d8dd9fcbf3bc46
Alberto <albertomaronezio@hotmai.com>
2019-05-28 16:42:56 -0300

    Exercicio03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 28;
+            int y = 43;
+            Console.WriteLine("Produto dos números: {0}", x * y);
+        }
+    }
+}
